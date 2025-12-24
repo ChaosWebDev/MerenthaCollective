7ccaea73db88856ddf471a0ce12894016b5a64e7
jpgerber1986 <jordangerber@gmail.com>
2025-12-24 13:33:51 -0700

    Init

---
+++ src/data/Items/index.js
@@ -0,0 +1,14 @@
+const itemsData = [
+  {
+    name: 'Newbie Sword',
+    subarea: 'Cabeiri',
+    type: 'weapon',
+  },
+  {
+    name: 'Newbie Armor',
+    subarea: 'Cabeiri',
+    type: 'armor',
+  },
+];
+
+export default itemsData;
