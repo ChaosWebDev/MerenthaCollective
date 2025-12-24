67e531b14306c69437fd7999d8a49179bb336a74
jpgerber1986 <jordangerber@gmail.com>
2025-12-24 14:49:14 -0700

    Add more data

--- src/data/Items/index.js
+++ src/data/Items/index.js
@@ -1,14 +1,58 @@
-const itemsData = [
+/**
+ * Level Values
+ *
+ * 1: Newbie
+ * 2: Mid
+ * 3: HM
+ * 4: Elite
+ * 5: Legendary
+ */
+
+const items = [
+  {
+    name: 'Crystal Ball',
+    subarea: 'Cabeiri',
+    details: 'Newbie level magic defense item.',
+    isQuest: true,
+    level: 1,
+  },
+  {
+    name: 'Blue Eyes',
+    subarea: 'Mystic Forest',
+    details: 'HM level magic defense item.',
+    isQuest: true,
+    level: 3,
+  },
+  {
+    name: 'Black Swirling Orb',
+    subarea: 'Holgresh',
+    details: 'Elite level magic defense item.',
+    isQuest: false,
+    level: 4,
+  },
+  {
+    name: 'Irredescent Pearls',
+    subarea: '',
+    details: 'Legandary (120+) level magic defense item.',
+    isQuest: false,
+    level: 5,
+  },
+].sort((a, b) => a.level - b.level || a.number - b.number);
+
+const weapons = [
   {
     name: 'Newbie Sword',
     subarea: 'Cabeiri',
-    type: 'weapon',
+    details: 'Newbie item upon character creation',
   },
+].sort((a, b) => a.level - b.level || a.number - b.number);
+
+const armor = [
   {
     name: 'Newbie Armor',
     subarea: 'Cabeiri',
-    type: 'armor',
+    details: 'Newbie item upon character creation',
   },
-];
+].sort((a, b) => a.level - b.level || a.number - b.number);
 
-export default itemsData;
+export { items, weapons, armor };
