3bbf65915b9af74c2d58b646f9cff8e72d548288
Jordan Gerber <jordangerber@gmail.com>
2026-08-07 15:43:39 -0600

    Cleanup

--- src/routes/Pages/Guides/Gmcp/index.js
+++ src/routes/Pages/Guides/Gmcp/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useParams, useHistory, Redirect } from 'react-router';
 import GridContainer from '@jumbo/components/GridContainer';
 import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
 import Grid from '@mui/material/Grid';
-import { Typography, Box, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import CmtCard from '@coremat/CmtCard';
 import CmtCardContent from '@coremat/CmtCard/CmtCardContent';
 import CmtCardHeader from '@coremat/CmtCard/CmtCardHeader';
-import Code from 'common/Code';
 
 import GMCPGuideData from 'data/Guides/GMCP';
 
